758f1f31127a41a1f706149fc0c76ccc1268b9c8
fitzpatrickluke <fitzpatrickluke10@gmail.com>
2021-11-15 18:29:45 +0000

    created basic loop and switch statement in main_1

--- app_1/main_1.cpp
+++ app_1/main_1.cpp
@@ -3,6 +3,10 @@
 #include <filesystem>
 using namespace std;
 
+// temporary comment, remove befor turning in
+// use this command to run the code
+// g++ --std=c++17 main_1.cpp ../code_1/miniGit.cpp -o a.out
+
 /*
  * Purpose; displays a menu with options
  */
@@ -20,7 +24,63 @@ void displayMenu()
     cout << "+-----------------------+" << endl;
     cout << "#> ";
 }
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
+    bool menuOn = true;
+    int menuChoice = 0;
+    
+    MiniGit myGit;
+    string fileName = "";
+    
+    while(menuOn)
+    {
+        displayMenu();
+        
+        cin >> menuChoice;
+        while(menuChoice <= 0 || menuChoice > 3)
+        {
+            cout << "Enter a valid optiond: ";
+            cin >> menuChoice;
+        }
+        
+        switch(menuChoice)
+        {
+            // initialise a new repository
+            case 1:
+                myGit.init(5);
+            break;
+                
+            // add files to current commit
+            case 2:
+                fileName = "";
+                cout << "Enter a file name: ";
+                cin >> fileName;
+                
+                myGit.add(fileName);
+            break;
+                /*
+            // remove files from current commit
+            case 3:
+            break;
+                
+            // Commit Changes
+            case 4:
+            break;
+                
+            // Search commits based on key word
+            case 5:
+            break;
+                
+            // check out specific version based on a unique commit option
+            case 6:
+            break;
+                */
+            // quit
+            case 3:
+                menuOn = false;
+            break;
+        }
+    }
    
     return 0;
 }
