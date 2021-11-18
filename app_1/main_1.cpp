c50e8b7c1b614dc07c7f640fc277614e6cf1caa8
mach7839coloradoedu <89550837+mach7839coloradoedu@users.noreply.github.com>
2021-11-17 17:47:26 -0700

    Update main_1.cpp

--- app_1/main_1.cpp
+++ app_1/main_1.cpp
@@ -32,7 +32,7 @@ bool checkCommitMessage(string message)
     for(int length = 0; message.size() > length; length++)
         if(message[length] == ' ')
             words++;
-    if(words > 2)
+    if(words > 2 || node)
         return false;
     return true;
 }
@@ -127,4 +127,3 @@ int main(int argc, char* argv[])
    
     return 0;
 }
-
